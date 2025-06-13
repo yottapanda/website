602019d3332e71a5a7a6dfba7476d7fce0f11214
Keval Kapdee <keval@kapdee.uk>
2025-06-13 21:09:24 +0100

    Add rehype mermaid

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import pagefind from "astro-pagefind";
 import tailwindcss from "@tailwindcss/vite";
+import rehypeMermaid from "rehype-mermaid";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,8 +13,14 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   markdown: {
+    syntaxHighlight: {
+      excludeLangs: ["mermaid"],
+    },
     shikiConfig: {
       theme: "css-variables",
     },
+    rehypePlugins: [[rehypeMermaid, {
+      strategy: "img-svg"
+    }]],
   },
 });
